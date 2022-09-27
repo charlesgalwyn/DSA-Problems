efed7a5cae909a0acf11ffd6bf326274dff3f96c
charlesgalwyn <charlesgalwyn@gmail.com>
2022-09-27 19:33:23 +0530

    check palindrome validity

---
+++ A Valid Palindrome/valid_palindrome.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args) {
+      String s = " A man, a plan, a canal: Panama ";
+      if (check(s)){
+        System.out.println("Yes");
+      }
+      else {
+        System.out.println("No");
+      }
+      
+    }
+    public static boolean check(String s){
+      s=s.toLowerCase();
+      //to replace all alphanumerics characters
+      s=s.replaceAll("[^a-z0-9]","");
+      for (int i=0,j=s.length()-1;i<j;i++,j--){
+        if (!(s.charAt(i)==s.charAt(j))){
+          return false;
+        }
+      }
+      return true;
+    }
+  }
