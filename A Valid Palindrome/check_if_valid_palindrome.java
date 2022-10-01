1fac7f7808d31f1315657516b0df998378ffb137
charlesgalwyn <charlesgalwyn@gmail.com>
2022-10-01 12:46:03 +0530

    check phrase palindrome or not

--- A Valid Palindrome/check_if_valid_palindrome.java
+++ A Valid Palindrome/check_if_valid_palindrome.java
@@ -1,5 +1,5 @@
 class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
       String s = " A man, a plan, a canal: Panama ";
       if (check(s)){
         System.out.println("Yes");
@@ -11,7 +11,7 @@ class Main {
     }
     public static boolean check(String s){
       s=s.toLowerCase();
-      //to replace all alphanumerics characters
+      //to replace the characters 
       s=s.replaceAll("[^a-z0-9]","");
       for (int i=0,j=s.length()-1;i<j;i++,j--){
         if (!(s.charAt(i)==s.charAt(j))){
