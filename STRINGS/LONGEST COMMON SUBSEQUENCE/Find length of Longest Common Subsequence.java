eebe857e6a12a49a04bdb58d36734ac6f4a82048
charlesgalwyn <charlesgalwyn@gmail.com>
2022-11-07 00:06:59 +0530

    length of longest common subsequence

---
+++ STRINGS/LONGEST COMMON SUBSEQUENCE/Find length of Longest Common Subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestCommonSubsequence(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }
+}
